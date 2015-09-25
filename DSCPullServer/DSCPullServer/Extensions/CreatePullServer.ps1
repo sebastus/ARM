857f69a1a1e6597b7caf6f93383e42e89905a2ea
sebastus <divingclimber@gmail.com>
2015-09-25 03:46:38 -0700

    create DSC pull server

---
+++ DSCPullServer/DSCPullServer/Extensions/CreatePullServer.ps1
@@ -0,0 +1,58 @@
+#
+# CreatePullServer.ps1
+#
+# DSC configuration for Pull Server and Compliance Server
+# Prerequisite: Certificate "CN=PSDSCPullServerCert" in "CERT:\LocalMachine\MY\" store
+# Note: A Certificate may be generated using MakeCert.exe: http://msdn.microsoft.com/en-us/library/windows/desktop/aa386968%28v=vs.85%29.aspx
+# this configuration originates here: https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/dev/Examples/Sample_xDscWebService.ps1
+#
+configuration CreatePullServer
+{
+
+    Import-DSCResource -ModuleName xPSDesiredStateConfiguration
+
+	# clean up DSC Certs from the store
+	Get-ChildItem CERT:\LocalMachine\MY\ | where {$_.Subject -eq "CN=PSDSCPullServerCert"} | Remove-Item
+
+	# Generate a self-signed cert and deploy it in CERT:\LocalMachine\MY
+	$scriptFolder = Split-Path $script:MyInvocation.MyCommand.Path
+	$makeCertPath = "$scriptFolder\MakeCert.exe"
+	& $makeCertPath -r -pe -n "CN=PSDSCPullServerCert" -ss my -sr localMachine
+	$psDscCert = Get-ChildItem CERT:\LocalMachine\MY\ | where {$_.Subject -eq "CN=PSDSCPullServerCert"}
+	$certificateThumbPrint = $psDscCert.Thumbprint
+
+    Node $AllNodes.NodeName
+    {
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = "Present"
+            Name   = "DSC-Service"            
+        }
+
+        xDscWebService PSDSCPullServer
+        {
+            Ensure                  = "Present"
+            EndpointName            = "PSDSCPullServer"
+            Port                    = 8080
+            PhysicalPath            = "$env:SystemDrive\inetpub\PSDSCPullServer"
+            CertificateThumbPrint   = $certificateThumbPrint         
+            ModulePath              = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+            ConfigurationPath       = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"            
+            State                   = "Started"
+            DependsOn               = "[WindowsFeature]DSCServiceFeature"                        
+        }
+
+        xDscWebService PSDSCComplianceServer
+        {
+            Ensure                  = "Present"
+            EndpointName            = "PSDSCComplianceServer"
+            Port                    = 9080
+            PhysicalPath            = "$env:SystemDrive\inetpub\PSDSCComplianceServer"
+            CertificateThumbPrint   = "AllowUnencryptedTraffic"
+            State                   = "Started"
+            IsComplianceServer      = $true
+            DependsOn               = @("[WindowsFeature]DSCServiceFeature","[xDSCWebService]PSDSCPullServer")
+        }
+    }
+ }
+
