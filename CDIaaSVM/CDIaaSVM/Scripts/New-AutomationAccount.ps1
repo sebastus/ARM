0996c1d2cef1c2e976b954ecb0f65e4f36433a71
Greg Oliver <golive@microsoft.com>
2015-10-17 11:45:49 -0700

    adjust script to agree with article changes

--- CDIaaSVM/CDIaaSVM/Scripts/New-AutomationAccount.ps1
+++ CDIaaSVM/CDIaaSVM/Scripts/New-AutomationAccount.ps1
@@ -1,11 +1,12 @@
 #
 # New_AutomationAccount.ps1
 #
+# Assumes Azure PowerShell v0.8.0 - v0.9.8
 Switch-AzureMode -Name AzureResourceManager 
 
-# gain access to Microsoft.Automation resource provider and feature set (do this once)
-Register-AzureProvider –ProviderNamespace Microsoft.Automation 
-Register-AzureProviderFeature -FeatureName dsc -ProviderNamespace Microsoft.Automation 
+# gain access to Microsoft.Automation resource provider and feature set (10/8/2015 - no longer necessary)
+# Register-AzureProvider –ProviderNamespace Microsoft.Automation 
+# Register-AzureProviderFeature -FeatureName dsc -ProviderNamespace Microsoft.Automation 
 
 # Create a resource group to contain your automation account
 New-AzureResourceGroup –Name MyAutomationRG –Location “East US 2” 
